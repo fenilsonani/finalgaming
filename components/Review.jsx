b2fffbd1e9f33fe8855fdf0cff4fb215827c3f85
Fenil <107786545+fenilsonani@users.noreply.github.com>
2023-10-14 02:21:10 +0530

    added some working functionality into the buttons to the company.js

--- components/Review.jsx
+++ components/Review.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cookies from "js-cookie";
 
 function Review() {
     return (
@@ -6,22 +7,34 @@ function Review() {
             <div className="container mx-auto px-4">
                 <div className='flex justify-evenly flex-wrap'>
                     <div className='w-full md:w-[30%]'>
-                        <img src="img/review_1.png" alt="" className='mx-auto' />
+                        <img src="img/review_1.png" alt="" className='mx-auto'/>
                     </div>
                     <div className='w-full md:w-[70%]'>
-                        <div className='flex justify-between flex-col md:flex-row items-center md:items-start gap-5 md:gap-0 pr-8 py-5'>
+                        <div
+                            className='flex justify-between flex-col md:flex-row items-center md:items-start gap-5 md:gap-0 pr-8 py-5'>
                             <div className='flex flex-col items-center'>
                                 <h3>Steve Cabanillas</h3>
                                 <p className='font-thin text-[#979797]'>Lenevo Group, UK</p>
                             </div>
                             <img src="img/lenovo_logo.png" alt=""/>
                         </div>
-                        <h4 className='py-10 tracking-wide leading-8'><img src="img/Vector.png" alt="" className='inline-block mr-2'/>
-                        Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla amsan, risus sem sollicitudin lacus, ut interdum Maecenas eget condimentum velit, sit amet
+                        <h4 className='py-10 tracking-wide leading-8'><img src="img/Vector.png" alt=""
+                                                                           className='inline-block mr-2'/>
+                            Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum
+                            est a, mattis tellus. Sed dignissim, metus nec fringilla amsan, risus sem sollicitudin
+                            lacus, ut interdum Maecenas eget condimentum velit, sit amet
                         </h4>
                     </div>
                 </div>
-        <button type="button" className="py-2.5 block mx-auto  px-5 text-sm font-medium bg-[#414141]  focus:outline-none text-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Know More</button>
+                <button type="button"
+                        onClick={() => {
+                            Cookies.set("pageAllowed", "3");
+                            localStorage.setItem("pageAllowed", "3");
+                            window.location.href = "/contactstart";
+                        }}
+                        className="py-2.5 block mx-auto  px-5 text-sm font-medium bg-[#414141]  focus:outline-none text-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Know
+                    More
+                </button>
 
             </div>
         </div>
--- components/Solution.jsx
+++ components/Solution.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+import Cookies from "js-cookie";
 
 function Solution() {
     const [soultionData, Setsolution] = useState(
@@ -20,32 +21,42 @@ function Solution() {
             },
         ]
     )
-  return (
-    <div className='w-full py-10'>
-      <div className="container mx-auto px-2 md:px-0">
-        <h2 className='text-center text-[#414141] text-2xl font-semibold py-2'>Modern Solutions we Provide</h2>
-        <p className='text-center font-thin'>Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent.</p>
-        <div className='grid grid-cols-2 md:grid-cols-4 gap-5 py-5'>
-            {
-                soultionData.map(
-                    (data, index) =>{
-                        return(
-                            <div className='w-full bg-white border-2 rounded py-5 px-2 mb-2 md:mb-0'
-                                 key={index}
-                            >
-                                <img src={data.icon} alt={data.icon} className='mx-auto' />
-                                <h3 className='text-center py-2'>{data.title}</h3>
-                                <p className='text-center font-thin'>{data.subhading}</p>
-                            </div>
+    return (
+        <div className='w-full py-10'>
+            <div className="container mx-auto px-2 md:px-0">
+                <h2 className='text-center text-[#414141] text-2xl font-semibold py-2'>Modern Solutions we Provide</h2>
+                <p className='text-center font-thin'>Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                    vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora
+                    torquent.</p>
+                <div className='grid grid-cols-2 md:grid-cols-4 gap-5 py-5'>
+                    {
+                        soultionData.map(
+                            (data, index) => {
+                                return (
+                                    <div className='w-full bg-white border-2 rounded py-5 px-2 mb-2 md:mb-0'
+                                         key={index}
+                                    >
+                                        <img src={data.icon} alt={data.icon} className='mx-auto'/>
+                                        <h3 className='text-center py-2'>{data.title}</h3>
+                                        <p className='text-center font-thin'>{data.subhading}</p>
+                                    </div>
+                                )
+                            }
                         )
                     }
-                )
-            }
+                </div>
+                <button type="button"
+                        className="py-2.5 block mx-auto  px-5 text-sm font-medium bg-[#414141]  focus:outline-none text-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                        onClick={() => {
+                            Cookies.set("pageAllowed", "3");
+                            localStorage.setItem("pageAllowed", "3");
+                            window.location.href = "/contactstart";
+                        }}
+                >Know More
+                </button>
+            </div>
         </div>
-        <button type="button" className="py-2.5 block mx-auto  px-5 text-sm font-medium bg-[#414141]  focus:outline-none text-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Know More</button>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Solution
