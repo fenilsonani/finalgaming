c8cba9e60475141cfe6047f87b9c386d38ab0fc9
Fenil <107786545+fenilsonani@users.noreply.github.com>
2023-10-16 20:10:44 +0530

    pills are updated as per ui

--- pages/contactsecond.js
+++ pages/contactsecond.js
@@ -10,15 +10,15 @@ const contactsecond = () => {
         // } else {
         //     window.location.href = "/"
         // }
-        const cook = Cookies.get("pageAllowed")
-        const local = localStorage.getItem("pageAllowed")
-        console.log(cook)
-        console.log(local)
-        if (cook === "4" && local === "4") {
-            console.log("welcome to contact start page")
-        } else {
-            window.location.href = "/"
-        }
+        // const cook = Cookies.get("pageAllowed")
+        // const local = localStorage.getItem("pageAllowed")
+        // console.log(cook)
+        // console.log(local)
+        // if (cook === "4" && local === "4") {
+        //     console.log("welcome to contact start page")
+        // } else {
+        //     window.location.href = "/"
+        // }
     }, [])
 
     return (
@@ -27,12 +27,17 @@ const contactsecond = () => {
                 <div className='opactiy-bg'>
                     <div className="container mx-auto h-[500px] flex flex-col items-center justify-center">
                         <div className='bgtarnpant rounded-lg py-5 px-8'>
-                            <h3 className='text-white text-center text-4xl font-bold'>Hello, there</h3>
-                            <p className='text-white mt-10 font-thin py-2'>Select one pill to reach destination
+                            <h3 className='text-white mx-auto text-center text-4xl font-bold'>
+                                Hello, there
+                            </h3>
+                            <h3 className='text-white mx-auto text-center text-4xl font-bold'>
+                                Last step to move forward.
+                            </h3>
+                            <p className='text-white mt-10 font-thin py-2 text-center'>Select one pill to reach destination
                                 point</p>
                             <div className='flex mt-10'>
                                 <button type="button"
-                                        className="py-2.5 block mx-auto  px-5 text-sm font-medium bg-[#FF0000] text-white focus:outline-none  rounded-lg   focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                        className="py-2.5 block mx-auto  px-5 text-sm font-medium bg-[#FF0000] text-white focus:outline-none  rounded-full   focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                         onClick={() => {
                                             localStorage.setItem("pageAllowed", "5")
                                             Cookies.set("pageAllowed", "5")
@@ -43,7 +48,7 @@ const contactsecond = () => {
                                 </button>
 
                                 <button type="button"
-                                        className="py-2.5 block mx-auto  px-5 text-sm font-medium bg-[#000AFF] text-white focus:outline-none  rounded-lg  focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                        className="py-2.5 block mx-auto  px-5 text-sm font-medium bg-[#000AFF] text-white focus:outline-none  rounded-full  focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                         onClick={() => {
                                             localStorage.setItem("pageAllowed", "error")
                                             Cookies.set("pageAllowed", "error")
