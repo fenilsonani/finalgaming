f64de1e8cee45c1e04023e54faed4a23f2ee48a4
Fenil <107786545+fenilsonani@users.noreply.github.com>
2023-10-18 00:16:18 +0530

    updated slider

--- components/Truseted.jsx
+++ components/Truseted.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function Truseted() {
     return (
-        <div className='w-fullbg-[#f2f2f2] py-10 '>
+        <div className='w-full bg-[#f2f2f2] py-10 '>
             <div className="container mx-auto">
                 {/*<h2 className='text-center py-3 text-xl md:text-2xl font-bold'>60+ Trusted by Government Entites and  Steering Organiztions</h2>*/}
-                <img src={"/60.svg"} className={"mx-auto mb-40 block md:hidden"} alt="60+"/>
-                <img src={"/60sm.svg"} className={"mx-auto mb-40 hidden md:block"} alt="60+"/>
-                <div className='flex justify-evenly bg-gray-100 py-5 w-full overflow-y-auto gap-10'>
+                <img src={"/60.svg"} className={"mx-auto mb-20 block md:hidden"} alt="60+"/>
+                <img src={"/60sm.svg"} className={"mx-auto mb-20 hidden md:block"} alt="60+"/>
+                <div className='flex justify-evenly bg-gray-100 py-5 w-full overflow-y-auto gap-16'>
                     <img src="img/turested/asana.png" alt="asanalogo"/>
                     <img src="img/turested/ghost.png" alt="ghostlogo"/>
                     <img src="img/turested/plaid.png" alt="plaidlogo"/>
