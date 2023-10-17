15a54eb657b09d062223fbd19569ff3735b4e90f
Fenil <107786545+fenilsonani@users.noreply.github.com>
2023-10-18 00:13:41 +0530

    footer updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div className='w-full bg-gradient-to-r from-black via-black to-gray-700 py-10'>
+        <div className='w-full bg-gradient-to-r from-black via-black  to-gray-700 py-10'>
             <div className="container mx-auto px-4">
-                <div className='flex'>
-                    <div className='w-full'>
+                <div className='grid grid-cols-12 gap-10'>
+                    <div className='w-full col-span-3'>
                         <img src="img/ftr-loog.png" alt="logo" className='mx-auto max-w-[100%]'/>
                     </div>
-                    <div className='text-white w-full'>
+                    <div className='text-white w-full col-span-9'>
                         <div className='flex gap-20 py-2'>
-                            <h3 className='font-["Orbitron",sans-serif] underline'>Cookie preferences</h3>
-                            <h3 className='font-["Orbitron",sans-serif]  underline'>Privacy policy</h3>
+                            <h3 className='font-["Orbitron",sans-serif] text-[10px] md:text-base underline'>Cookie preferences</h3>
+                            <h3 className='font-["Orbitron",sans-serif] text-[10px] md:text-base  underline'>Privacy policy</h3>
                         </div>
-                        <p className='py-3 font-["Orbitron",sans-serif] text-sm font-thin'>Jorem ipsum dolor sit amet,
+                        <p className='py-3 font-["Orbitron",sans-serif] text-[10px] md:text-base font-thin'>Jorem ipsum dolor sit amet,
                             consectetur adipiscing elit. All rights reserved</p>
                     </div>
                 </div>
